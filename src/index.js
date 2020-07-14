d8f85d22e4da3c3a8394f28705c34ccbe759b919
Sergey Ermolaev <ro1sergey.se@gmail.com>
2020-07-14 11:23:37 +0300

    Init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// JS
+import './js/'
+import '@fortawesome/fontawesome-free/js/all.js';
+import 'slick-carousel/slick/slick.min.js'
+
+// jquery
+// Added in webpack.base.conf.js
+
+// SCSS
+import './assets/scss/main.scss'
+import 'slick-carousel/slick/slick.scss'
+import 'slick-carousel/slick/slick-theme.scss'
+
+// CSS (example)
+//import './assets/css/flexboxgrid2.css'
+import 'flexboxgrid2/flexboxgrid2.css'
+import 'normalize.css'
+
+// Vue.js
+// window.Vue = require('vue')
+
+// Vue components (for use in html)
+// Vue.component('example-component', require('./components/Example.vue').default)
+
+// Vue init
+// const app = new Vue({
+//   el: '#app'
+// })
